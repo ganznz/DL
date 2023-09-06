28bf902c6c6fc0aa8f610fea93d5318894b62427
Logan <loganbellinghamlb@gmail.com>
2023-09-07 09:58:24 +1200

    Minor changes

--- src/ReplicatedStorage/PlayerData/Template.lua
+++ src/ReplicatedStorage/PlayerData/Template.lua
@@ -1,6 +1,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local GenreConfig = require(ReplicatedStorage.Configs:WaitForChild("Genre"))
+local TopicConfig = require(ReplicatedStorage.Configs:WaitForChild("Topic"))
 
 -- Template is what empty profiles default to
 local Template = {
@@ -28,10 +29,12 @@ local Template = {
         Genres = {
             Action = GenreConfig.new('Action'),
             Casual = GenreConfig.new('Casual'),
-            Strategy = GenreConfig.new('Strategy')
+            Strategy = GenreConfig.new('Strategy'),
         },
         Topics = {
-            
+            Modern = TopicConfig.new("Modern"),
+            Medieval = TopicConfig.new("Medieval"),
+            SciFi = TopicConfig.new("SciFi"),
         }
     }
 }
