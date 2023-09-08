7f30e6e2ff620a0a38cb392c4440ea42da7a4c87
Logan <loganbellinghamlb@gmail.com>
2023-09-09 01:19:28 +1200

    Furniture store teleportation implemented

---
+++ src/ServerScriptService/FurnitureStore.server.lua
@@ -0,0 +1,27 @@
+local Workspace = game:GetService("Workspace")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Zone = require(ReplicatedStorage.Libs.Zone)
+
+local Remotes = ReplicatedStorage.Remotes
+
+local furnitureStoreExterior: Model = Workspace.Map.Buildings.FurnitureStore:WaitForChild("FurnitureStoreExterior")
+local hitbox: Model = furnitureStoreExterior:WaitForChild("Hitbox")
+local zone = Zone.new(hitbox)
+
+local furnitureStoreInterior = Workspace.Map.Buildings.FurnitureStore:WaitForChild("FurnitureStoreInterior")
+local furnitureStoreInteriorTeleport = furnitureStoreInterior:FindFirstChild("Teleport")
+
+
+zone.playerEntered:Connect(function(plr: Player)
+    Remotes.GUI.ChangeGuiStatusRemote:FireClient(plr, "loadingScreen")
+    Remotes.GUI.FreezePlayer:FireClient(plr)
+end)
+
+Remotes.Player.TeleportPlr.OnServerEvent:Connect(function(plr, destination)
+    if destination == "furnitureStore" then
+        local char = plr.Character
+        char:MoveTo(furnitureStoreInteriorTeleport.Position)
+    end
+    
+end)
