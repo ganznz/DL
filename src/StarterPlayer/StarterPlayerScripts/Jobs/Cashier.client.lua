30bc1802fbe08e5650844e1fc92ae6e5c113fe3d
Logan <loganbellinghamlb@gmail.com>
2023-09-25 21:45:32 +1300

    Pathfinding fixed + added customer order billboard GUI

--- src/StarterPlayer/StarterPlayerScripts/Jobs/Cashier.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/Jobs/Cashier.client.lua
@@ -1,19 +1,43 @@
 local Workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local PathfindingService = game:GetService("PathfindingService")
+local Players = game:GetService("Players")
 
+local localPlr = Players.LocalPlayer
+local PlayerGui = localPlr.PlayerGui
 local Remotes = ReplicatedStorage.Remotes
 
 local npcSpawnPart = Workspace.Map.Buildings.IceCreamStore:WaitForChild("IceCreamStoreInterior"):WaitForChild("NpcSpawnPart")
 local npcDestinationPart = Workspace.Map.Buildings.IceCreamStore:WaitForChild("IceCreamStoreInterior"):WaitForChild("NpcDestinationPart")
 
-local path = PathfindingService:CreatePath()
-local waypoints
-local nextWaypointIndex
-local reachedConnection
-local blockedConnection
+-- gui
+local customerOrderBillboard = PlayerGui:WaitForChild("Jobs").CashierJob.CashierJobCustomerOrder:WaitForChild("CustomerOrderBillboard")
 
-local function followPath(npcHumanoid: Humanoid)
+local function displayCustomerOrder(humanoid, customerInfo)
+    local npcModelHead = humanoid.Parent:FindFirstChild("Head")
+    local billboardGui = customerOrderBillboard:Clone()
+    billboardGui.Parent = humanoid.Parent
+    local orderContainerGui = billboardGui.CustomerOrderContainer
+    billboardGui.Adornee = npcModelHead
+
+    if customerInfo.icecream == "Chocolate" then
+        orderContainerGui.BackgroundColor3 = Color3.fromRGB(141, 96, 66)
+    elseif customerInfo.icecream == "Vanilla" then
+        orderContainerGui.BackgroundColor3 = Color3.fromRGB(255, 251, 236)
+    elseif customerInfo.icecream == "Strawberry" then
+        orderContainerGui.BackgroundColor3 = Color3.fromRGB(255, 162, 162)
+    end
+
+    billboardGui.Enabled = true
+end
+
+local function followPath(npcHumanoid: Humanoid, customerInfo)
+    local path = PathfindingService:CreatePath()
+    local waypoints
+    local nextWaypointIndex
+    local reachedConnection
+    local blockedConnection
+
     local success, errorMessage = pcall(function()
         path:ComputeAsync(npcSpawnPart.Position, npcDestinationPart.Position)
     end)
@@ -24,6 +48,7 @@ local function followPath(npcHumanoid: Humanoid)
         blockedConnection = path.Blocked:Connect(function(blockedWaypointIndex)
             if blockedWaypointIndex >= nextWaypointIndex then
                 blockedConnection:Disconnect()
+                blockedConnection = nil
 
                 -- recompute new path
                 followPath(npcHumanoid)
@@ -32,6 +57,7 @@ local function followPath(npcHumanoid: Humanoid)
 
         -- detect when movement to next waypoint is complete
         if not reachedConnection then
+            print(nextWaypointIndex)
             reachedConnection = npcHumanoid.MoveToFinished:Connect(function(reached)
                 if reached and nextWaypointIndex < #waypoints then
                     -- move to next waypoint
@@ -40,6 +66,9 @@ local function followPath(npcHumanoid: Humanoid)
                 else
                     reachedConnection:Disconnect()
 					blockedConnection:Disconnect()
+                    reachedConnection = nil
+                    blockedConnection = nil
+                    displayCustomerOrder(npcHumanoid, customerInfo)
                 end
             end)
         end
@@ -58,5 +87,5 @@ Remotes.Jobs.Cashier.SendCustomer.OnClientEvent:Connect(function(customerInfo)
     customerModel.Parent = Workspace
     customerModel.PrimaryPart.Position = npcSpawnPart.Position
 
-    followPath(customerHumanoid)
+    followPath(customerHumanoid, customerInfo)
 end)
