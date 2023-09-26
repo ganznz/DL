62e186cb0a26ab93ac2ebd82a2efe120336b2998
Logan <loganbellinghamlb@gmail.com>
2023-09-26 21:24:33 +1300

    Added customerl lifecycle coroutine

--- src/StarterPlayer/StarterPlayerScripts/Jobs/Cashier.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/Jobs/Cashier.client.lua
@@ -1,11 +1,15 @@
+local UserInputService = game:GetService("UserInputService")
 local Workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local PathfindingService = game:GetService("PathfindingService")
 local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
 
 local localPlr = Players.LocalPlayer
 local PlayerGui = localPlr.PlayerGui
 local Remotes = ReplicatedStorage.Remotes
+
+local mouse = localPlr:GetMouse()
 
 local npcSpawnPart = Workspace.Map.Buildings.IceCreamStore:WaitForChild("IceCreamStoreInterior"):WaitForChild("NpcSpawnPart")
 local npcDestinationPart = Workspace.Map.Buildings.IceCreamStore:WaitForChild("IceCreamStoreInterior"):WaitForChild("NpcDestinationPart")
@@ -13,10 +17,10 @@ local npcDestinationPart = Workspace.Map.Buildings.IceCreamStore:WaitForChild("I
 -- gui
 local customerOrderBillboard = PlayerGui:WaitForChild("Jobs").CashierJob.CashierJobCustomerOrder:WaitForChild("CustomerOrderBillboard")
 
-local function displayCustomerOrder(humanoid, customerInfo)
-    local npcModelHead = humanoid.Parent:FindFirstChild("Head")
+local function displayCustomerOrder(customerModel: Model, customerInfo)
+    local npcModelHead = customerModel:FindFirstChild("Head")
     local billboardGui = customerOrderBillboard:Clone()
-    billboardGui.Parent = humanoid.Parent
+    billboardGui.Parent = customerModel
     local orderContainerGui = billboardGui.CustomerOrderContainer
     billboardGui.Adornee = npcModelHead
 
@@ -31,7 +35,26 @@ local function displayCustomerOrder(humanoid, customerInfo)
     billboardGui.Enabled = true
 end
 
-local function followPath(npcHumanoid: Humanoid, customerInfo)
+local function customerInteraction(customerModel: Model)
+    mouse.Button1Down:Connect(function()
+        local target = mouse.Target
+        if target:IsDescendantOf(customerModel) then
+            customerModel:SetAttribute("orderFulfilled", true)
+        end
+    end)
+end
+
+local customerLifecycle = coroutine.create(function(customerModel: Model, customerInfo)
+    displayCustomerOrder(customerModel, customerInfo)
+    customerInteraction(customerModel)
+    print('a')
+    coroutine.yield()
+    print('b')
+
+end)
+
+local function followPath(customerModel: Model, customerInfo)
+    local humanoid = customerModel:FindFirstChild("Humanoid")
     local path = PathfindingService:CreatePath()
     local waypoints
     local nextWaypointIndex
@@ -51,31 +74,33 @@ local function followPath(npcHumanoid: Humanoid, customerInfo)
                 blockedConnection = nil
 
                 -- recompute new path
-                followPath(npcHumanoid)
+                followPath(humanoid)
             end
         end)
 
         -- detect when movement to next waypoint is complete
         if not reachedConnection then
-            print(nextWaypointIndex)
-            reachedConnection = npcHumanoid.MoveToFinished:Connect(function(reached)
+            reachedConnection = humanoid.MoveToFinished:Connect(function(reached)
                 if reached and nextWaypointIndex < #waypoints then
                     -- move to next waypoint
                     nextWaypointIndex += 1
-                    npcHumanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+                    humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
                 else
                     reachedConnection:Disconnect()
 					blockedConnection:Disconnect()
-                    reachedConnection = nil
-                    blockedConnection = nil
-                    displayCustomerOrder(npcHumanoid, customerInfo)
+
+                    coroutine.resume(customerLifecycle, customerModel, customerInfo)
+                    
+                    customerModel:GetAttributeChangedSignal("orderFulfilled"):Connect(function()
+                        coroutine.resume(customerLifecycle, customerModel, customerInfo)
+                    end)
                 end
             end)
         end
 
         -- start moving NPC
         nextWaypointIndex = 2
-        npcHumanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+        humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
     else
         warn("NPC path not computed.", errorMessage)
     end
@@ -83,9 +108,8 @@ end
 
 Remotes.Jobs.Cashier.SendCustomer.OnClientEvent:Connect(function(customerInfo)
     local customerModel = ReplicatedStorage.Assets.Models:FindFirstChild("NPC"):Clone()
-    local customerHumanoid = customerModel:FindFirstChild("Humanoid")
     customerModel.Parent = Workspace
     customerModel.PrimaryPart.Position = npcSpawnPart.Position
 
-    followPath(customerHumanoid, customerInfo)
+    followPath(customerModel, customerInfo)
 end)
