0d9911f3d0b855feea3a21ab0bbbf8e32409a733
Logan <loganbellinghamlb@gmail.com>
2023-09-28 19:19:09 +1300

    Implemented job time remaining

--- src/ReplicatedStorage/Utils/Gui/GuiServices.lua
+++ src/ReplicatedStorage/Utils/Gui/GuiServices.lua
@@ -5,32 +5,47 @@ local GuiTransparency = require(ReplicatedStorage.Libs:WaitForChild("GUITranspar
 
 local GuiServices = {}
 
-function GuiServices.DefaultMainGuiStyling(guiInstance: Frame)
-    guiInstance.Position = UDim2.fromScale(0.5, guiInstance.Position.Y.Scale + 0.3)
+function GuiServices.DefaultMainGuiStyling(guiInstance: Frame, posOffset: number)
+    guiInstance.Position = UDim2.fromScale(0.5, guiInstance.Position.Y.Scale + posOffset)
     guiInstance.Visible = false
     GuiTransparency:SetTransparency(guiInstance, 1, TweenInfo.new(0))
 end
 
-function GuiServices.ShowGuiStandard(guiInstance: Frame, goalPos, goalSize)
+function GuiServices.ShowGuiStandard(guiInstance: Frame, goalPos, goalSize, opacityTween: boolean)
     guiInstance.Visible = true
     local tweenInfo = TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out)
     local mainTween = TweenService:Create(guiInstance, tweenInfo, {
         Position = goalPos,
         Size = goalSize
     })
+
+    if opacityTween then
+        GuiTransparency:SetTransparency(guiInstance, 0, TweenInfo.new(0.2))
+    else
+        GuiTransparency:SetTransparency(guiInstance, 0, TweenInfo.new(0))
+    end
+
     mainTween:Play()
-    GuiTransparency:SetTransparency(guiInstance, 0, TweenInfo.new(0.2))
 end
 
-function GuiServices.HideGuiStandard(guiInstance: Frame, goalPos, goalSize)
+function GuiServices.HideGuiStandard(guiInstance: Frame, goalPos, goalSize, opacityTween: boolean)
     local tweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out)
     local mainTween = TweenService:Create(guiInstance, tweenInfo, {
         Position = goalPos,
         Size = goalSize
     })
     mainTween:Play()
-    GuiTransparency:SetTransparency(guiInstance, 1, TweenInfo.new(0.2))
-    task.delay(0.2, function() guiInstance.Visible = false end)
+
+    if opacityTween then
+        GuiTransparency:SetTransparency(guiInstance, 1, TweenInfo.new(0.2))
+    end
+
+    task.delay(0.5, function()
+        guiInstance.Visible = false
+        if not opacityTween then
+            GuiTransparency:SetTransparency(guiInstance, 1, TweenInfo.new(0))
+        end
+    end)
 end
 
 return GuiServices
