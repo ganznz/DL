1786f0960a833cdceea9b71e72d93db1877e32a8
Logan <loganbellinghamlb@gmail.com>
2023-10-12 18:48:17 +1300

    Minor changes

--- src/ReplicatedStorage/GlobalVariables.lua
+++ src/ReplicatedStorage/GlobalVariables.lua
@@ -5,6 +5,6 @@ GlobalVariables.Gui = {}
 GlobalVariables.Gui.MainGuiInvisiblePosOffset = 1
 GlobalVariables.Gui.ValidGreenColour = Color3.fromRGB(93, 217, 91)
 GlobalVariables.Gui.InvalidGreyColour = Color3.fromRGB(210, 210, 210)
-GlobalVariables.Gui.CantAffordColour = Color3.fromRGB(255, 206, 131)
+GlobalVariables.Gui.CantAffordColour = Color3.fromRGB(241, 170, 63)
 
 return GlobalVariables
