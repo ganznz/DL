21b5dffd953a1a8b9ada4fb4546b21ebf5739eff
Logan <loganbellinghamlb@gmail.com>
2023-10-18 22:00:13 +1300

    Studio functionality

---
+++ src/ServerScriptService/Functionality/Studio/Studio.lua
@@ -0,0 +1,42 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local PlrDataManager = require(ServerScriptService.PlayerData.Manager)
+local StudioConfig = require(ReplicatedStorage.Configs.Studio)
+
+local Remotes = ReplicatedStorage.Remotes
+
+local Studio = {}
+
+function Studio.PurchaseNextStudio(plr: Player)
+    local profile = PlrDataManager.Profiles[plr]
+    if not profile then return end
+
+    local plrData = profile.Data
+
+    local currentPlrStudioLevel = StudioConfig.GetPlrStudioLevel(plrData)
+
+    -- check if plr already has every studio unlocked
+    if StudioConfig.HasLastStudio(plrData) then return "You already own every studio!" end
+
+    local nextPlrStudioLevel = currentPlrStudioLevel + 1
+    local nextStudioConfig = StudioConfig.GetConfig(nextPlrStudioLevel)
+
+    -- attempt to purchase studio
+    local plrCash = plrData.Cash
+    local studioUpgradePrice = nextStudioConfig.Price
+
+    local canAfford = StudioConfig.CanPurchaseNextStudio(plrData)
+    if not canAfford then return "You need " .. tostring(studioUpgradePrice - plrCash) .. " more cash!" end
+
+    -- can afford, purchase studio
+    PlrDataManager.AdjustPlrCash(plr, -studioUpgradePrice)
+    profile.Data.Studio.CurrentActiveStudio = nextPlrStudioLevel
+    table.insert(profile.Data.Studio.Studios, { Furnishings = {} })
+
+    Remotes.Purchase.PurchaseComputer:FireClient(plr, nextPlrStudioLevel)
+    return "Purchased " .. nextStudioConfig.Name .. "!"
+
+end
+
+return Studio
---
+++ src/StarterPlayer/StarterPlayerScripts/Jobs/Studio/Studio.client.lua
@@ -0,0 +1,2 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
