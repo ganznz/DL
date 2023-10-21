651c3f993f52d66f1ac34d3e6c7d66b3c540f787
Logan <loganbellinghamlb@gmail.com>
2023-10-21 20:28:15 +1300

    Added new Gui variable

--- src/ReplicatedStorage/GlobalVariables.lua
+++ src/ReplicatedStorage/GlobalVariables.lua
@@ -7,5 +7,6 @@ GlobalVariables.Gui.LoadingBgTweenTime = 0.5
 GlobalVariables.Gui.ValidGreenColour = Color3.fromRGB(93, 217, 91)
 GlobalVariables.Gui.InvalidGreyColour = Color3.fromRGB(210, 210, 210)
 GlobalVariables.Gui.CantAffordColour = Color3.fromRGB(241, 170, 63)
+GlobalVariables.Gui.InvalidRedColour = Color3.fromRGB(255, 106, 108)
 
 return GlobalVariables
