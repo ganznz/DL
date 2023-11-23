251699867dd259f66faa0009606bb7a946e4aa85
Logan <loganbellinghamlb@gmail.com>
2023-11-23 19:19:39 +1300

    Genre & Topic objects now compatible w/ data serialization

--- src/ReplicatedStorage/Configs/Genre.lua
+++ src/ReplicatedStorage/Configs/Genre.lua
@@ -1,22 +1,42 @@
-local export genreTypes = {
-    "Action", "Casual", "Strategy", "Rythm", "Simulation", "Adventure", "Platformer", "Shooter", "Sports", "Fighting", "Roleplay",
-    "Racing", "Arcade", "Roguelite", "Puzzle", "Tower Defense",
-}
-
 local Genre = {}
 Genre.__index = Genre
 
-function Genre.new(name: string)
-    if not table.find(genreTypes, name) then return nil end
+Genre.Genres = {
+    ["Action"] = { Image = "" },
+    ["Casual"] = { Image = "" },
+    ["Strategy"] = { Image = "" },
+    ["Rythm"] = { Image = "" },
+    ["Simulation"] = { Image = "" },
+    ["Adventure"] = { Image = "" },
+    ["Platformer"] = { Image = "" },
+    ["Sports"] = { Image = "" },
+    ["Fighting"] = { Image = "" },
+    ["Roleplay"] = { Image = "" },
+    ["Racing"] = { Image = "" },
+    ["Arcade"] = { Image = "" },
+    ["Roguelite"] = { Image = "" },
+    ["Puzzle"] = { Image = "" },
+    ["Tower Defense"] = { Image = "" },
+}
+
+function Genre.new(name: string, level: number | nil, xp: number | nil, compatibleTopic: string | nil, incompatibleTopic: string | nil)
+    if not Genre.Genres[name] then return nil end
 
     local newGenre = {}
     setmetatable(newGenre, Genre)
 
     newGenre.Name = name
-    newGenre.compatibleWith = {}
-    newGenre.incompatibleWith = {}
+    newGenre.Level = level or 1
+    newGenre.XP = xp or 0
+    newGenre.CompatibleWith = compatibleTopic or nil
+    newGenre.IncompatibleWith = incompatibleTopic or nil
 
     return newGenre
+end
+
+function Genre.GetImage(name: string): string
+    local genreInfo = Genre.Genres[name]
+    if genreInfo then return genreInfo.Image else return "" end
 end
 
 -- CLASS METHODS
@@ -28,16 +48,16 @@ end
 
 -- INSTANCE METHODS
 function Genre:AddCompatibleTopic(topicObject)
-    if not table.find(self.compatibleWith, topicObject) and not table.find(self.incompatibleWith, topicObject) then
-        table.insert(self.compatibleWith, topicObject)
-        topicObject:AddCompatibleGenre(self)
+    -- if both genre and topic don't have compatibilities
+    if not self.CompatibleWith and not topicObject.CompatibleWith then
+        self.CompatibleWith = topicObject.Name
     end
 end
 
 function Genre:AddIncompatibleTopic(topicObject)
-    if not table.find(self.incompatibleWith, topicObject) and not table.find(self.compatibleWith, topicObject) then
-        table.insert(self.incompatibleWith, topicObject)
-        topicObject:AddIncompatibleGenre(self)
+    -- if both genre and topic don't have incompatibilities
+    if not self.IncompatibleWith and not topicObject.IncompatibleWith then
+        self.IncompatibleWith = topicObject.Name
     end
 end
 
--- src/ReplicatedStorage/Configs/Topic.lua
+++ src/ReplicatedStorage/Configs/Topic.lua
@@ -1,21 +1,45 @@
-local export topicTypes = {
-    "Modern", "Medieval", "SciFi", "Space", "Dance", "Classical", "War", "Romance",
-    "Fantasy", "Mystery", "Horror", "Superheroes", "Cyberpunk", "Post Apocalyptic", "Zombies",
-    "Crime", "Fairy Tale", "Wildlife", "Farming", "Underwater", "Disaster",
-}
-
 local Topic = {}
 Topic.__index = Topic
 
-function Topic.new(name: string)
+Topic.Topics = {
+    ["Modern"] = { Image = "" },
+    ["Medieval"] = { Image = "rbxassetid://15321891852" },
+    ["Space"] = { Image = "rbxassetid://15322151139" },
+    ["Classical"] = { Image = "" },
+    ["War"] = { Image = "rbxassetid://15322175017" },
+    ["Romance"] = { Image = "rbxassetid://15321900358" },
+    ["Fantasy"] = { Image = "rbxassetid://15321873100" },
+    ["Mystery"] = { Image = "" },
+    ["Horror"] = { Image = "rbxassetid://15321880408" },
+    ["Superheroes"] = { Image = "" },
+    ["Cyberpunk"] = { Image = "" },
+    ["Zombies"] = { Image = "rbxassetid://15322197034" },
+    ["Crime"] = { Image = "" },
+    ["Fairy Tale"] = { Image = "" },
+    ["Wildlife"] = { Image = "rbxassetid://15322190945" },
+    ["Farming"] = { Image = "" },
+    ["Underwater"] = { Image = "rbxassetid://15322164872" },
+    ["Disaster"] = { Image = "rbxassetid://15321826005" },
+}
+
+function Topic.new(name: string, level: number | nil, xp: number | nil, compatibleGenre: string | nil, incompatibleGenre: string | nil)
+    if not Topic.Topics[name] then return nil end
+
     local newTopic = {}
     setmetatable(newTopic, Topic)
 
     newTopic.Name = name
-    newTopic.compatibleWith = {}
-    newTopic.incompatibleWith = {}
+    newTopic.Level = level or 1
+    newTopic.XP = xp or 0
+    newTopic.compatibleWith = compatibleGenre or nil
+    newTopic.incompatibleWith = incompatibleGenre or nil
 
     return newTopic
+end
+
+function Topic.GetImage(name: string): string
+    local topicInfo = Topic.Topics[name]
+    if topicInfo then return topicInfo.Image else return "" end
 end
 
 -- CLASS METHODS
@@ -27,14 +51,16 @@ end
 
 -- INSTANCE METHODS
 function Topic:AddCompatibleGenre(genreObject)
-    if not table.find(self.compatibleWith, genreObject) and not table.find(self.incompatibleWith, genreObject) then
-        table.insert(self.compatibleWith, genreObject)
+    -- if both genre and topic don't have compatibilities
+    if not self.compatibleWith and not genreObject.compatibleWith then
+        self.compatibleWith = genreObject.Name
     end
 end
 
 function Topic:AddIncompatibleGenre(genreObject)
-    if not table.find(self.incompatibleWith, genreObject) and not table.find(self.compatibleWith, genreObject) then
-        table.insert(self.incompatibleWith, genreObject)
+    -- if both genre and topic don't have incompatibilities
+    if not self.incompatibleWith and not genreObject.incompatibleWith then
+        self.incompatibleWith = genreObject.Name
     end
 end
 
