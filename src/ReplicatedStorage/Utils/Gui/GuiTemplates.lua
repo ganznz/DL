2415e8fa56f58765fce62fc1a5d586fe1ed879bf
Logan <loganbellinghamlb@gmail.com>
2023-12-22 09:01:29 +1300

    Added Button template

---
+++ src/ReplicatedStorage/Utils/Gui/GuiTemplates.lua
@@ -0,0 +1,27 @@
+local TweenService = game:GetService("TweenService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local GlobalVariables = require(ReplicatedStorage:WaitForChild("GlobalVariables"))
+
+local GuiTemplates = {}
+
+function GuiTemplates.CreateButton(btn: Instance, rotates: boolean?)
+    local originalRotation = btn.Rotation
+    local tweenMouseEnter = TweenService:Create(btn, TweenInfo.new(0.2), { Rotation = 5 })
+    local tweenMouseLeave = TweenService:Create(btn, TweenInfo.new(0.2), { Rotation = originalRotation })
+
+    btn.MouseEnter:Connect(function()
+        if rotates then tweenMouseEnter:Play() end
+        GlobalVariables.Sound.Sfx.GuiMouseHover:Play()
+    end)
+
+    btn.MouseLeave:Connect(function()
+        if rotates then tweenMouseLeave:Play() end
+    end)
+
+    btn.Activated:Connect(function()
+        GlobalVariables.Sound.Sfx.GuiOpen:Play()
+    end)
+end
+
+return GuiTemplates
