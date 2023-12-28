2d47f4101992cc9cf7851d3978391cf11ba934b4
Logan <loganbellinghamlb@gmail.com>
2023-12-28 23:21:27 +1300

    Added GetChanceTable method + minor changes

--- src/ReplicatedStorage/Configs/Phones/Phones.lua
+++ src/ReplicatedStorage/Configs/Phones/Phones.lua
@@ -3,6 +3,8 @@ local Phones = {}
 export type PhoneConfig = {
     Price: number,
     Currency: string,
+    ImageFront: string,
+    ImagePerspective: string,
     Unlockables: {
         Materials: {
             [string]: { -- consumable name
@@ -26,7 +28,10 @@ local config: { [string]: PhoneConfig } = {
     ["Basic"] = {
         Price = 1000,
         Currency = "Coins",
+        ImageFront = "15775845654",
+        ImagePerspective = "15776089669",
         Unlockables = {
+            -- Chance has to add to 100 across all items
             Materials = {
                 -- 20% total
                 ["Metal Scrap"] = { Chance = 10 },
@@ -52,6 +57,31 @@ Phones.Config = config
 
 function Phones.GetConfig(phoneName: string): PhoneConfig | nil
     return Phones.Config[phoneName]
+end
+
+function Phones.GetChanceTable(phoneName: string): {}
+    local currentRangeMax = 0
+
+    -- { itemName = { UnlockableType: string, Bounds: {} } }
+    local percentages = {}
+
+    local phoneConfig = Phones.GetConfig(phoneName)
+    if not phoneConfig then return nil end
+
+    for unlockableType, unlockables in phoneConfig.Unlockables do
+        for itemName, itemInfo in unlockables do
+            local minBound = currentRangeMax
+            currentRangeMax += itemInfo.Chance
+            local maxBound = currentRangeMax
+
+            percentages[itemName] = {
+                UnlockableType = unlockableType,
+                Bounds = {minBound, maxBound}
+            }
+        end
+    end
+
+    return percentages
 end
 
 function Phones.GetTotalItemChance(phoneName: string, unlockableType: "Staff" | "Consumables" | "Materials"): number
