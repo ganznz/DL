1525edab5933cbb7ec1b40356aea9626a588f982
Logan <loganbellinghamlb@gmail.com>
2023-12-31 09:27:55 +1300

    Added GetCFrameOrientationmethod

--- src/ReplicatedStorage/Utils/GeneralUtils.lua
+++ src/ReplicatedStorage/Utils/GeneralUtils.lua
@@ -1,3 +1,5 @@
+-- // this file contains methods that can be used anywhere without dependancy on gameplay code // --
+
 local GeneralUtils = {}
 
 function GeneralUtils.HasProperty(object, propertyName)
@@ -39,6 +41,21 @@ end
 
 function GeneralUtils.GetDecalUrl(imageID: string)
     return string.format("http://www.roblox.com/asset/?id=%s", imageID)
+end
+
+function GeneralUtils.GetCFrameOrientation(cframe: CFrame): {}
+    local sx, sy, sz, m00, m01, m02, m10, m11, m12, m20, m21, m22 = cframe:GetComponents()
+    local orientation = {}
+    
+    local x = math.atan2(-m12, m22)
+    local y = math.asin(m02)
+    local z = math.atan2(-m01, m00)
+    
+    orientation["X"] = x
+    orientation["Y"] = y
+    orientation["Z"] = z
+
+    return orientation
 end
 
 return GeneralUtils
