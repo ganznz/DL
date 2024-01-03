6da8252b35b880076ccd2caab3e93c3e60482b50
Logan <loganbellinghamlb@gmail.com>
2024-01-04 12:36:27 +1300

    Updated lockItem method to support staff members

--- src/ServerScriptService/Functionality/Inventory/Inventory.server.lua
+++ src/ServerScriptService/Functionality/Inventory/Inventory.server.lua
@@ -11,15 +11,24 @@ local function lockItem(plr: Player, itemType: string, itemInfo: {})
     local profile = PlrDataManager.Profiles[plr]
     if not profile then return end
 
+    local itemUUID
     local itemInstance
     if itemType == "furniture" then
         itemInstance = profile.Data.Inventory.StudioFurnishing[itemInfo.ItemCategory][itemInfo.ItemName][itemInfo.ItemUUID]
         if not itemInstance then return end
 
+        itemUUID = itemInfo.ItemUUID
         itemInstance.Locked = not itemInstance.Locked
+    
+    elseif itemType == "staff" then
+        itemInstance = profile.Data.Inventory.StaffMembers[itemInfo.UUID]
+        if not itemInstance then return end
 
-        Remotes.Inventory.General.LockItem:FireClient(plr, "furniture", itemInfo, itemInstance.Locked)
+        itemUUID = itemInfo.UUID
+        itemInstance.Locked = not itemInstance.Locked
     end
+
+    Remotes.Inventory.General.LockItem:FireClient(plr, itemType, itemUUID, itemInstance.Locked)
 end
 
 Remotes.Inventory.General.LockItem.OnServerEvent:Connect(lockItem)
