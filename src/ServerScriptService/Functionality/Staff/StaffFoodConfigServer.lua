5f02abef7d7526ed743efc215fc0ee15e0f0403b
Logan <loganbellinghamlb@gmail.com>
2024-01-05 14:27:43 +1300

    Added DeleteFood method

--- src/ServerScriptService/Functionality/Staff/StaffFoodConfigServer.lua
+++ src/ServerScriptService/Functionality/Staff/StaffFoodConfigServer.lua
@@ -15,4 +15,17 @@ function StaffFoodServer.GiveFood(plr: Player, foodName: string)
     end
 end
 
+function StaffFoodServer.DeleteFood(plr: Player, foodName: string, amtToDelete: number)
+    local profile = PlrDataManager.Profiles[plr]
+    if not profile then return end
+
+    -- safety check in case amtToDelete value is greater than actual inventory amount value.
+    if amtToDelete > profile.Data.Inventory.StaffFood[foodName].Amount then
+        profile.Data.Inventory.StaffFood[foodName].Amount = 0
+        return
+    end
+
+    profile.Data.Inventory.StaffFood[foodName].Amount -= amtToDelete
+end
+
 return StaffFoodServer
