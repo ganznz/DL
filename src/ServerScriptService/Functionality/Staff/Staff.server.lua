b07219c0eb35c5672e513eb0407bb9a0b1e3b2a3
Logan <loganbellinghamlb@gmail.com>
2024-01-10 14:19:02 +1300

    Staff training now functional

--- src/ServerScriptService/Functionality/Staff/Staff.server.lua
+++ src/ServerScriptService/Functionality/Staff/Staff.server.lua
@@ -2,8 +2,16 @@ local ServerScriptService = game:GetService("ServerScriptService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local PlrDataManager = require(ServerScriptService.PlayerData.Manager)
+local StaffMemberConfig = require(ReplicatedStorage.Configs.Staff.StaffMember)
+local StaffMemberServer = require(ServerScriptService.Functionality.Staff.StaffMemberServer)
 
 local Remotes = ReplicatedStorage.Remotes
+
+-- intermediary function that directs remote call to server-sided instance method
+local function levelUpStaffMemberSkill(plr: Player, staffMemberUUID: string, skill: "code" | "sound" | "art", amtOfLvlUps: number)
+    StaffMemberServer:LevelUpSkill(plr, staffMemberUUID, skill, amtOfLvlUps)
+
+end
 
 Remotes.Staff.GetStaffMemberData.OnServerInvoke = function(plr: Player, staffMemberUUID: string): {} | nil
     local profile = PlrDataManager.Profiles[plr]
@@ -11,3 +19,5 @@ Remotes.Staff.GetStaffMemberData.OnServerInvoke = function(plr: Player, staffMem
 
     return profile.Data.Inventory.StaffMembers[staffMemberUUID]
 end
+
+Remotes.Staff.LevelUpSkill.OnServerEvent:Connect(levelUpStaffMemberSkill)
--- src/ServerScriptService/Functionality/Staff/StaffMemberServer.lua
+++ src/ServerScriptService/Functionality/Staff/StaffMemberServer.lua
@@ -1,12 +1,51 @@
 -- // The functionality in this file are used only for operations on StaffMember class objects // --
 -- general staff member functionality (e.g. unlocking staff member, deleting staff member) in StaffServer.lua
 
+local Players = game:GetService("Players")
 local ServerScriptService = game:GetService("ServerScriptService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+local PlrDataManager = require(ServerScriptService.PlayerData.Manager)
+local StaffMember = require(ReplicatedStorage.Configs.Staff.StaffMember)
+
 local Remotes = ReplicatedStorage.Remotes
 
-local StaffMember = require(ReplicatedStorage.Configs.Staff.StaffMember)
+-- function cannot be called directly from client, rather a remote connects a ServerEvent which then calls the function.
+function StaffMember:LevelUpSkill(plr: Player, staffMemberUUID: string, skill: "code" | "sound" | "art", amtOfLvlUps: number)
+    local profile = PlrDataManager.Profiles[plr]
+    if not profile then return end
+
+    local staffMemberData = profile.Data.Inventory.StaffMembers[staffMemberUUID]
+    if not staffMemberData then return end
+
+    local staffMemberInstance = StaffMember.new(staffMemberUUID, staffMemberData)
+    local staffMemberConfig = StaffMember.GetConfig(profile.Data.Inventory.StaffMembers[staffMemberUUID].Model)
+
+    local plrCurrencyAmt = profile.Data[staffMemberConfig.UpgradeCurrency]
+    local upgradeCost = staffMemberInstance:CalcSkillLevelUpgradeCost(skill, amtOfLvlUps)
+    if plrCurrencyAmt < upgradeCost then return end
+
+    -- all checks passed, level up skill accordingly
+    if skill == "code" then
+        staffMemberInstance.CodeLevel += amtOfLvlUps -- update instance data
+        profile.Data.Inventory.StaffMembers[staffMemberUUID].CodeLevel += amtOfLvlUps -- update database data
+    
+    elseif skill == "sound" then
+        staffMemberInstance.SoundLevel += amtOfLvlUps
+        profile.Data.Inventory.StaffMembers[staffMemberUUID].SoundLevel += amtOfLvlUps
+
+    elseif skill == "art" then
+        staffMemberInstance.ArtistLevel += amtOfLvlUps
+        profile.Data.Inventory.StaffMembers[staffMemberUUID].ArtistLevel += amtOfLvlUps
+    end
+
+    if staffMemberConfig.UpgradeCurrency == "Coins" then
+        PlrDataManager.AdjustPlrCoins(plr, -upgradeCost)
+    end
+
+    -- replicate changes to client
+    Remotes.Staff.LevelUpSkill:FireClient(plr, staffMemberUUID, skill)
+end
 
 function StaffMember:AdjustEnergy(amt: number)
     local newAmt = self.CurrentEnergy + amt
