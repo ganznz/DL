c836c1d89f892c52189e9202a8e18f43b3606a23
Logan <loganbellinghamlb@gmail.com>
2024-01-21 11:15:32 +1300

    Changes to configs

--- src/ReplicatedStorage/Configs/GameDev/Genre.lua
+++ src/ReplicatedStorage/Configs/GameDev/Genre.lua
@@ -1,26 +1,38 @@
 local Genre = {}
 Genre.__index = Genre
 
-Genre.Genres = {
-    ["Action"] = { Image = "" },
-    ["Casual"] = { Image = "" },
-    ["Strategy"] = { Image = "" },
-    ["Rythm"] = { Image = "" },
-    ["Simulation"] = { Image = "" },
-    ["Adventure"] = { Image = "" },
-    ["Platformer"] = { Image = "" },
-    ["Sports"] = { Image = "" },
-    ["Fighting"] = { Image = "" },
-    ["Roleplay"] = { Image = "" },
-    ["Racing"] = { Image = "" },
-    ["Arcade"] = { Image = "" },
-    ["Roguelite"] = { Image = "" },
-    ["Puzzle"] = { Image = "" },
-    ["Tower Defense"] = { Image = "" },
+export type GenreConfig = {
+    ImageIcon: string,
+    ImageSplash: string
 }
 
+local config: { [string]: GenreConfig } = {
+    ["Action"] = { ImageIcon = "", ImageSplash = "" },
+    ["Casual"] = { ImageIcon = "", ImageSplash = "" },
+    ["Strategy"] = { ImageIcon = "", ImageSplash = "" },
+    ["Rythm"] = { ImageIcon = "", ImageSplash = "" },
+    ["Simulation"] = { ImageIcon = "", ImageSplash = "" },
+    ["Adventure"] = { ImageIcon = "", ImageSplash = "" },
+    ["Platformer"] = { ImageIcon = "", ImageSplash = "" },
+    ["Sports"] = { ImageIcon = "", ImageSplash = "" },
+    ["Fighting"] = { ImageIcon = "", ImageSplash = "" },
+    ["Roleplay"] = { ImageIcon = "", ImageSplash = "" },
+    ["Racing"] = { ImageIcon = "", ImageSplash = "" },
+    ["Arcade"] = { ImageIcon = "", ImageSplash = "" },
+    ["Roguelite"] = { ImageIcon = "", ImageSplash = "" },
+    ["Puzzle"] = { ImageIcon = "", ImageSplash = "" },
+    ["Tower Defense"] = { ImageIcon = "", ImageSplash = "" },
+}
+
+Genre.Config = config
+
+function Genre.GetConfig(name: string): GenreConfig | nil
+    return Genre.Config[name]
+end
+
 function Genre.new(name: string, level: number | nil, xp: number | nil, compatibleTopic: string | nil, incompatibleTopic: string | nil)
-    if not Genre.Genres[name] then return nil end
+    local genreConfig = Genre.GetConfig(name)
+    if not genreConfig then return nil end
 
     local newGenre = {}
     setmetatable(newGenre, Genre)
@@ -32,11 +44,6 @@ function Genre.new(name: string, level: number | nil, xp: number | nil, compatib
     newGenre.IncompatibleWith = incompatibleTopic or nil
 
     return newGenre
-end
-
-function Genre.GetImage(name: string): string
-    local genreInfo = Genre.Genres[name]
-    if genreInfo then return genreInfo.Image else return "" end
 end
 
 -- CLASS METHODS
--- src/ReplicatedStorage/Configs/GameDev/Topic.lua
+++ src/ReplicatedStorage/Configs/GameDev/Topic.lua
@@ -1,29 +1,42 @@
 local Topic = {}
 Topic.__index = Topic
 
-Topic.Topics = {
-    ["Modern"] = { Image = "" },
-    ["Medieval"] = { Image = "rbxassetid://15321891852" },
-    ["Space"] = { Image = "rbxassetid://15322151139" },
-    ["Classical"] = { Image = "rbxassetid://15500603910" },
-    ["War"] = { Image = "rbxassetid://15322175017" },
-    ["Romance"] = { Image = "rbxassetid://15321900358" },
-    ["Fantasy"] = { Image = "rbxassetid://15321873100" },
-    ["Mystery"] = { Image = "rbxassetid://15500607232" },
-    ["Horror"] = { Image = "rbxassetid://15321880408" },
-    ["Superhero"] = { Image = "rbxassetid://15500663476" },
-    ["Cyberpunk"] = { Image = "rbxassetid://15500676919" },
-    ["Zombies"] = { Image = "rbxassetid://15322197034" },
-    ["Crime"] = { Image = "rbxassetid://15500821014" },
-    ["Fairy Tale"] = { Image = "rbxassetid://15500831419" },
-    ["Wildlife"] = { Image = "rbxassetid://15322190945" },
-    ["Farming"] = { Image = "rbxassetid://15500840746" },
-    ["Underwater"] = { Image = "rbxassetid://15322164872" },
-    ["Disaster"] = { Image = "rbxassetid://15321826005" },
+export type TopicConfig = {
+    ImageIcon: string,
+    ImageSplash: string
 }
 
+
+local config: { [string]: TopicConfig } = {
+    ["Modern"] = { ImageIcon = "", ImageSplash = "" },
+    ["Medieval"] = { ImageIcon = "15321891852", ImageSplash = "16019379114" },
+    ["Space"] = { ImageIcon = "15322151139", ImageSplash = "16019372896" },
+    ["Classical"] = { ImageIcon = "15500603910", ImageSplash = "16018696449" },
+    ["War"] = { ImageIcon = "15322175017", ImageSplash = "16019376051" },
+    ["Romance"] = { ImageIcon = "15321900358", ImageSplash = "16019372124" },
+    ["Fantasy"] = { ImageIcon = "15321873100", ImageSplash = "16019347093" },
+    ["Mystery"] = { Image = "15500607232", ImageSplash = "16019370516" },
+    ["Horror"] = { ImageIcon = "15321880408", ImageSplash = "16019349187" },
+    ["Superhero"] = { ImageIcon = "15500663476", ImageSplash = "16019373783" },
+    ["Cyberpunk"] = { ImageIcon = "15500676919", ImageSplash = "16018697363" },
+    ["Zombies"] = { ImageIcon = "15322197034", ImageSplash = "16019378257" },
+    ["Crime"] = { ImageIcon = "15500821014", ImageSplash = "16018683567" },
+    ["Fairy Tale"] = { ImageIcon = "15500831419", ImageSplash = "16018700351" },
+    ["Wildlife"] = { ImageIcon = "15322190945", ImageSplash = "16019377005" },
+    ["Farming"] = { ImageIcon = "15500840746", ImageSplash = "16019348167" },
+    ["Underwater"] = { ImageIcon = "15322164872", ImageSplash = "16019375034" },
+    ["Disaster"] = { ImageIcon = "15321826005", ImageSplash = "16018698368" },
+}
+
+Topic.Config = config
+
+function Topic.GetConfig(name: string): TopicConfig | nil
+    return Topic.Config[name]
+end
+
 function Topic.new(name: string, level: number | nil, xp: number | nil, compatibleGenre: string | nil, incompatibleGenre: string | nil)
-    if not Topic.Topics[name] then return nil end
+    local topicConfig = Topic.GetConfig(name)
+    if not topicConfig then return nil end
 
     local newTopic = {}
     setmetatable(newTopic, Topic)
@@ -35,11 +48,6 @@ function Topic.new(name: string, level: number | nil, xp: number | nil, compatib
     newTopic.incompatibleWith = incompatibleGenre or nil
 
     return newTopic
-end
-
-function Topic.GetImage(name: string): string
-    local topicInfo = Topic.Topics[name]
-    if topicInfo then return topicInfo.Image else return "" end
 end
 
 -- CLASS METHODS
