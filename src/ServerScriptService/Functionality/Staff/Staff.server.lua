1c44124ed22b11dbf423960cc4c23c0f08e3dc14
Logan <loganbellinghamlb@gmail.com>
2024-01-24 15:19:50 +1300

    Fixed server-sided staff member instance methods not 'existing'/being recognized

--- src/ServerScriptService/Functionality/Staff/Staff.server.lua
+++ src/ServerScriptService/Functionality/Staff/Staff.server.lua
@@ -3,20 +3,27 @@ local ServerScriptService = game:GetService("ServerScriptService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local PlrDataManager = require(ServerScriptService.PlayerData.Manager)
-local StaffMemberConfig = require(ReplicatedStorage.Configs.Staff.StaffMember)
-local StaffMemberServer = require(ServerScriptService.Functionality.Staff.StaffMemberServer)
+local StudioConfig = require(ReplicatedStorage.Configs.Studio.Studio)
+local StudioConfigServer = require(ServerScriptService.Functionality.Staff.StaffMemberServer)
 
 local Remotes = ReplicatedStorage.Remotes
 
 local function adjustAllStaffMemberEnergy(plr: Player)
     local profile = PlrDataManager.Profiles[plr]
-        if not profile then return end
+    if not profile then return end
 
-        local staffData = profile.Data.Inventory.StaffMembers
-        for staffMemberUUID: string, staffMemberData in staffData do
-            local staffInstance = StaffMemberConfig.new(staffMemberUUID, staffMemberData)
-            staffInstance:AdjustEnergy(plr, staffMemberUUID)
-        end
+    local staffMembersToIgnore = {}
+    if plr:GetAttribute("InStudio") and plr:GetAttribute("CurrentlyDevelopingGame") then
+        staffMembersToIgnore = StudioConfig.GetStaffInActiveStudio(profile.Data)
+    end
+
+    for staffMemberUUID: string, staffMemberData in profile.Data.Inventory.StaffMembers do
+        if table.find(staffMembersToIgnore, staffMemberUUID) then continue end
+
+        local staffInstance = StudioConfigServer.new(staffMemberUUID, staffMemberData)
+
+        staffInstance:AdjustEnergy(plr, staffMemberUUID)
+    end
 end
 
 -- intermediary function that directs remote call to server-sided instance method
@@ -25,7 +32,7 @@ local function levelUpStaffMemberSkill(plr: Player, staffMemberUUID: string, ski
     if not profile then return end
 
     local instanceData = profile.Data.Inventory.StaffMembers[staffMemberUUID]
-    local instance = StaffMemberConfig.new(staffMemberUUID, instanceData)
+    local instance = StudioConfigServer.new(staffMemberUUID, instanceData)
 
     instance:LevelUpSkill(plr, staffMemberUUID, skill, amtOfLvlUps)
 end
