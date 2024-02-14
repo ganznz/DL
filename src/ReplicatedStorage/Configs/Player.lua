bb73e4fb216f2c86b32625c9129ee33a0b073c64
Logan <loganbellinghamlb@gmail.com>
2024-02-14 21:28:20 +1300

    Removed methods

--- src/ReplicatedStorage/Configs/Player.lua
+++ src/ReplicatedStorage/Configs/Player.lua
@@ -2,33 +2,14 @@ local Player = {}
 
 Player.XpPerLevel = 100
 
-function Player.GetXp(plrData): number
-    return plrData.Character.Exp
-end
-
-function Player.GetLevel(plrData): number
-    return plrData.Character.Level
-end
-
-function Player.GetCurrentHunger(plrData): number
-    return plrData.Character.Needs.CurrentHunger
-end
-
-function Player.GetCurrentEnergy(plrData): number
-    return plrData.Character.Needs.CurrentEnergy
-end
-
-function Player.GetCurrentMood(plrData): number
-    return plrData.Character.Needs.CurrentMood
-end
-
 function Player.CalcMaxNeed(plrData): number
     return 10 * math.pow(2, plrData.Character.Level - 1)
 end
 
 function Player.CalcLevelUpXpRequirement(plrData): number
-    local plrLevel = Player.GetLevel(plrData)
-    return if plrLevel == 1 then Player.XpPerLevel else Player.XpPerLevel * plrLevel
+    local plrLevel = plrData.Character.Level
+
+    return 10 * math.pow(1.35, plrLevel + 1)
 end
 
 
